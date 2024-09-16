a91722b89ef78f9558aad750bb1f5e47e079fe76
Neko0ld <smorlalov.andrey@gmail.com>
2024-09-16 11:08:35 +0300

    in work HeadsOfTheDepartment

---
+++ AvaloniaApplication1/AvaloniaApplication1/Classes/Order.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.Media;
+
+namespace AvaloniaApplication1.Classes;
+
+public class Order
+{
+    public int orderid { get; set; }
+    public string datecreation { get; set; }
+    public string orderstatus { get; set; }
+    public string nameconference { get; set; }
+    public int amountguests { get; set; }
+    public string equipment { get; set; }
+    public string paymentstatus { get; set; }
+}
--- AvaloniaApplication1/AvaloniaApplication1/Windows/HeadsOfTheDepartmentWindow.axaml.cs
+++ AvaloniaApplication1/AvaloniaApplication1/Windows/HeadsOfTheDepartmentWindow.axaml.cs
@@ -1,13 +1,49 @@
-﻿using Avalonia;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using AvaloniaApplication1.Classes;
+using Dapper;
+using MySqlConnector;
 
 namespace AvaloniaApplication1.Windows;
 
-public partial class HeadsOfTheDepartmentWindow : Window
+public partial class HeadsOfTheDepartmentWindow : Window, INotifyPropertyChanged
 {
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void Invalidate()
+    {
+        if (PropertyChanged != null)
+            PropertyChanged(this, new PropertyChangedEventArgs("userList"));
+    }
     public HeadsOfTheDepartmentWindow()
     {
+        // запрос на заполнение списка заказов
+        using (MySqlConnection database = new MySqlConnection(Globals.connectionString))
+        {
+            orderList = database.Query<Order>(
+                "SELECT * " +
+                "FROM `order`").ToList();
+        }
         InitializeComponent();
+    }
+    
+    // список заказов
+    private List<Order> _orderList = null;
+    public List<Order> orderList
+    {
+        get
+        {
+            var res = _orderList;
+
+            return res;
+        }
+        set
+        {
+            _orderList = value;
+            Invalidate();
+        }
     }
 }
